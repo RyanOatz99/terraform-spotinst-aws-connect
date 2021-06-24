b7b0a9a03a97fca95ee1e7d1e3f6f1a0c0844ba2
Steven Feltner <steven.feltner@netapp.com>
2021-06-23 19:08:35 -0700

    Update terraform to 0.13.0

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "name" {
-    description = "Name of the spotinst account to create."
-    type = string
-    default = null
+    type        = string
+    default     = null
+    description = "(OPTIONAL) Name for the Spot account. If none is provided will use the AWS Linked account display name for the Spot account name"
+
 }
 variable "profile" {
-    type = string
-    default = null
+    type        = string
+    default     = null
+    description = "(OPTIONAL) AWS profile name. Ex: default"
 }
