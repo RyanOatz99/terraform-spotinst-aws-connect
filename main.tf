5a31d3259fbc91de133bca3ca7634ab34bd47ded
Steven Feltner <steven.feltner@netapp.com>
2021-10-06 09:44:48 -0700

    Remove alias

--- main.tf
+++ main.tf
@@ -7,10 +7,11 @@ locals {
     cmd = "${path.module}/scripts/spot-account-aws"
     account_id = lookup(data.external.account.result,"account_id","Fail")
     external_id = data.external.external_id.result["external_id"]
-    name = var.name == null ? data.aws_iam_account_alias.current.account_alias : var.name
+    #name = var.name == null ? data.aws_iam_account_alias.current.account_alias : var.name
+    name = var.name
 }
 
-data "aws_iam_account_alias" "current" {}
+#data "aws_iam_account_alias" "current" {}
 
 # Create a random string
 resource "random_id" "role" {
